7efbbe15d204e1dda9664fd34889b66e234d7e84
Adam Hurm <adamhurm@users.noreply.github.com>
2022-08-30 18:43:03 -0400

    decouple animation-related functions

--- sonos/test-pixels.ts
+++ sonos/test-pixels.ts
@@ -1,12 +1,21 @@
 import { DisplayTransition, Glyph, GlyphAlignment } from '..'
+import { NuimoControlDevice } from '..'
 import { bootstrap, connectToDevice } from '../examples/utils'
 
 // Uncomment to search for only your device
 // ENTER DEVICE_ID to discover only a particular device`
 const DEVICE_ID: string | undefined = undefined
 
+function banner_buffer(banner: string[]) {
+    console.log("banner_buffer stub")
+}
+
 // This function creates a Glyph array for scrolling text animations.
-function scroll_text(banner: string[]): Glyph[] {
+// TODO: Create function to add buffer to beginning and end of Glyph for clean transition.
+function banner_to_animation(banner: string[], add_buffer = true): Glyph[] {
+    if (add_buffer) {
+        banner_buffer(banner)
+    }
     // total frames - 9 + 1 (becuase you need to have at least one frame)
     let framecount = banner[0].length - 8
     let animation = [] as Glyph[]
@@ -21,15 +30,15 @@ function scroll_text(banner: string[]): Glyph[] {
     return animation
 }
 
-/**
- * Main application entry point
- */
-async function main() {
-    const device = await connectToDevice(DEVICE_ID)
 
+async function startup_splash(device: NuimoControlDevice) {
     // Create a custom Glyph
-    // Nuimo has a 9x9 LED display, but you only need to create what you need
+    // Nuimo has a 9x9 LED display
+        // You only need to create what you need
     // and use `GlyphAlignment` to align the glyph
+    //
+    // In order to use scrolling banners larger than 9x9,
+    // send your Nx9 string to scroll_text(example_banner_string)
     //
     // For animated banner glyphs, I suggest adding leading and trailing whitespace.
     const sonos_string = [
@@ -44,27 +53,33 @@ async function main() {
         '                                           '
     ]
 
-    const animation: Glyph[] = scroll_text(sonos_string)
+    const animation: Glyph[] = banner_to_animation(sonos_string)
     device.displayGlyph(animation[0], {
                        alignment: GlyphAlignment.Center,
                        transition: DisplayTransition.CrossFade,
     })
 
-    let i = 0
+    let banner_frame = 0
     const interval = setInterval(() => {
         // Check if the client is still connected
         if (!device.isConnected) {
             return
         }
-        if (i >= animation.length) {
-            i = 0
+        /* // Loop animation
+        if (banner_frame >= animation.length) {
+                banner_frame = 0
         }
-        device.displayGlyph(animation[i], {
+        */
+        // Stop animation after one cycle
+        // TODO: Add whitespace buffer to banner automatically
+        if (banner_frame >= animation.length) {
+            return
+        }
+        device.displayGlyph(animation[banner_frame], {
                 alignment: GlyphAlignment.Center,
                 transition: DisplayTransition.Immediate,
         })
-        console.log(animation[i])
-        i = i + 1
+        banner_frame++
     }, 250)
 
     // If there is a disconnection, cancel the animation
@@ -72,6 +87,15 @@ async function main() {
         clearInterval(interval)
     })
 
+}
+
+/**
+ * Main application entry point
+ */
+async function main() {
+    const device = await connectToDevice(DEVICE_ID)
+
+    await startup_splash(device)
     /*
     // When display button is pressed
     device.on('selectDown', () => {
