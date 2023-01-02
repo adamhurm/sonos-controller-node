ee1f7a249037de234d2a201504db21a1a2013744
Adam Hurm <me@adamhurm.com>
2023-01-02 05:22:12 +0000

    code and comment cleanup

--- sonos/test-pixels.ts
+++ sonos/test-pixels.ts
@@ -22,56 +22,46 @@ function bannerAddBuffer(banner: string[]) {
 }
 
 // This function creates a Glyph array for scrolling text animations.
-// TODO: Create function to add buffer to beginning and end of Glyph for clean transition.
 function bannerToAnimation(banner: string[], addBuffer = false): Glyph[] {
     if (addBuffer) {
         banner = bannerAddBuffer(banner)
     }
-    // total frames - 9 + 1 (becuase you need to have at least one frame)
+    // N (total frames) - 9 (we have Nx9 array) + 1 (we have at least one frame)
     let frameCount = banner[0].length - 8
     let animation : Glyph[] = []
     for (let i = 0; i < frameCount; i++) {
         let frame : string[] = []
         for (let row = 0; row < 9; row++) {
             frame.push(banner[row].substring(i,i+9))
-            // console.log("Frame " + i + ", Row " + row + ": " + frame[row])
         }
         animation.push(Glyph.fromString(frame))
     }
     return animation
 }
 
-// This function converts a number 0-100 to a Glyph
+// This function converts a number to a double-digit Glyph
 function numberGlyph(n: number) {
+    // Only handle integers 0-100
     if ((n < 0) || (n > 100) || !Number.isInteger(n)) throw "outside of allowed range"
+    // Return special "100" Glyph
     if (n == 100) { return digitGlyph100 }
+    // Return combined Glyph of first and second digit
     return concatGlyph(digitGlyphsSmall[Math.floor((n/10) % 10)], digitGlyphsSmall[n % 10])
 }
 
 // This function concatenates two Glyphs, adding a space in between
 function concatGlyph(a: Glyph, b: Glyph): Glyph {
-    /*
-     * assert(a.characterRows.length === b.characterRows.length)
-     * assert(a.characterRows[0].length === b.characterRows[0].length)
-     */
+    if (a.characterRows.length != b.characterRows.length) throw "Glyph heights do not match"
     let combined = []
     for (let i = 0; i < a.characterRows.length; i++) {
-        combined.push(a.characterRows[i].concat(' ',b.characterRows[i]))
+        combined.push(a.characterRows[i].concat(' ', b.characterRows[i]))
     }
     return Glyph.fromString(combined)
 }
 
 // This function displays the "SONOS" startup splash screen
 async function startupSplash(device: NuimoControlDevice) {
-    // Create a custom 9x9 Glyph for Nuimo LED display
-    // You only need to create what you need
-    // and use `GlyphAlignment` to align the glyph
-    //
-    // In order to use scrolling banners longer than 9x9,
-    // send your Nx9 string to scroll_text(example_banner_string)
-    //
-    // For animated banner glyphs,
-    // add_buffer will add leading and trailing spaces
+    // Display beginning of animation Glyph on device
     const sonosString : string[] = [
         '                         ',
         ' **   **  *   *  **   ** ',
@@ -83,13 +73,13 @@ async function startupSplash(device: NuimoControlDevice) {
         ' **   **  *   *  **   ** ',
         '                         '
     ]
-
     const animation: Glyph[] = bannerToAnimation(sonosString, true)
     device.displayGlyph(animation[0], {
                        alignment: GlyphAlignment.Center,
                        transition: DisplayTransition.CrossFade,
     })
 
+    // Continue sending Glyph animations to device
     let bannerFrame = 0
     const interval = setInterval(() => {
         // Check if the client is still connected
@@ -111,8 +101,12 @@ async function startupSplash(device: NuimoControlDevice) {
         bannerFrame++
     }, 250)
 
-    // If there is a disconnection, cancel the animation
+    // If there is a disconnection, cancel the animation.
     device.on('disconnect', () => {
+        clearInterval(interval)
+    })
+    // If the display button is touched, cancel the animation.
+    device.on('touch', () => {
         clearInterval(interval)
     })
 
@@ -122,28 +116,30 @@ async function startupSplash(device: NuimoControlDevice) {
  * Main application entry point
  */
 async function main() {
+    // Get nuimo device and sonos speaker
     const device = await connectToDevice(DEVICE_ID)
+    const speaker = new Sonos('0.0.0.0')
 
     await startupSplash(device)
 
-    const speaker = new Sonos('0.0.0.0')
-
+    // Play + Pause when display button is clicked.
     device.on('select', async() => {
-	let status = await speaker.getCurrentState()
-	let toggleGlyph = emptyGlyph
-	if (status == "playing") {
-	    toggleGlyph = pauseGlyph
-	}
-	else if (status == "paused") {
-	    toggleGlyph = playGlyph
-	}
-	speaker.togglePlayback()
+        let status = await speaker.getCurrentState()
+        let toggleGlyph = emptyGlyph
+        if (status == "playing") {
+            toggleGlyph = pauseGlyph
+        }
+        else if (status == "paused") {
+            toggleGlyph = playGlyph
+        }
+        speaker.togglePlayback()
         device.displayGlyph(toggleGlyph, {
-	        alignment: GlyphAlignment.Center,
-	        transition: DisplayTransition.CrossFade,
-	})
+                alignment: GlyphAlignment.Center,
+                transition: DisplayTransition.CrossFade,
+        })
     })
-    // Show current volume when display button is pressed & released.
+
+    // Show current volume when display button is touched.
     device.on('touch', async() => {
         let volumeGlyph = numberGlyph(await speaker.getVolume())
         device.displayGlyph(volumeGlyph, {
@@ -156,28 +152,12 @@ async function main() {
                 transition: DisplayTransition.CrossFade,
         })
     })
-    
-    // Set the rotation mode to clamp, ensuring the rotation stops at a min/max value for us
-    device.rotationMode = RotationMode.Clamped
-
-    // When using clamped rotation you may set the min/max values using setRotationRange
-    // In this example the range is from -1 to 1, starting at 0 (mid-range)
-    // The final value represents the number of rotation cycles it takes between the min and max. By default
-    // this is the spread of the max-min (2 in this example). For example, this is set to 2 (1 positive and 1 negative rotation)
-    device.setRotationRange(-1, 1, 0, 2)
-
-    /*
-    // Rotation in any direction
-    device.on('rotate', (delta, rotation) => {
-        logEvent('rotate', {
-            delta,
-            rotation,
-        })
-    })
-    */
 
     // Volume Control
+    device.rotationMode = RotationMode.Clamped
+    device.setRotationRange(-1, 1, 0, 2)
     device.on('rotate', async(delta, rotation) => { 
+        // Linear Conversion (https://stackoverflow.com/a/929107/5024903)
         let newVolume = Math.floor(((rotation + 1) * 100) / 2)
         let volumeGlyph = numberGlyph(newVolume)
         
